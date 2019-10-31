b904d8be6a8b9895b78e1a69e0ccbc993a214d70
Alexander Costas <alexander.costas@mongodb.com>
2019-10-31 15:35:43 -0400

    intermediate

---
+++ buildscripts/signal_processing_setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [[ -d "signal_processing_venv" ]]; then
+    source ./signal_processing_venv/bin/activate
+    exit 0
+fi
+cat > ./analysis.yml << EOF
+mongo_uri: mongodb+srv://${dsi_analysis_atlas_user}:${dsi_analysis_atlas_pw}@performancedata-g6tsc.mongodb.net/perf
+is_patch: ${is_patch}
+task_id: ${task_id}
+EOF
+virtualenv -p /opt/mongodbtoolchain/v3/bin/python3 signal_processing_venv
+cat > ./signal_processing_venv/pip.conf << EOF
+[global]
+index-url = https://pypi.org/simple
+extra-index-url = https://${perf_jira_user}:${perf_jira_pw}@artifactory.corp.mongodb.com/artifactory/api/pypi/mongodb-dag-local/simple"
+EOF
+source ./signal_processing_venv/bin/activate
+pip install dag-signal-processing~=1.0
