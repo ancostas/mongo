c431824b2a574a0f504c8d13f4aa1c2361995a71
Alexander Costas <alexander.costas@mongodb.com>
2019-11-01 14:33:18 -0400

    updates

--- buildscripts/signal_processing_setup.sh
+++ buildscripts/signal_processing_setup.sh
@@ -4,7 +4,7 @@ if [[ -d "signal_processing_venv" ]]; then
     exit 0
 fi
 cat > ./analysis.yml << EOF
-mongo_uri: mongodb+srv://${dsi_analysis_atlas_user}:${dsi_analysis_atlas_pw}@performancedata-g6tsc.mongodb.net/perf
+mongo_uri: mongodb+srv://\${analysis_user}:\${analysis_password}@performancedata-g6tsc.mongodb.net/perf
 is_patch: ${is_patch}
 task_id: ${task_id}
 EOF
@@ -16,3 +16,6 @@ extra-index-url = https://${perf_jira_user}:${perf_jira_pw}@artifactory.corp.mon
 EOF
 source ./signal_processing_venv/bin/activate
 pip install dag-signal-processing~=1.0
+
+export analysis_user="${dsi_analysis_atlas_user}"
+export analysis_password="${dsi_analysis_atlas_pw}"
